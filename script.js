280dbfc73f500abdeb52a89d089ab0cdd578986d
Angri154 <79702542+Angri154@users.noreply.github.com>
2022-08-04 14:13:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let div = document.createElement('div');
+div.className = "alert";
+div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+document.body.append(div);
+function alerted(){
+    
+    
+    for(let x=0;x<=10;x++){
+        document.querySelector(".h1").innerText=x;
+        document.querySelector(",div").innerText=x;
+        console.log(x)
+        let i=0;
+        
+
+        
+    }
+  }
